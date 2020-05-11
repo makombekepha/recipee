33273a5bcfeab284efd454b281915f05b3717c55
makombekepha <makombekepha@gmail.com>
2020-05-11 21:12:15 +0300

    Updates

---
+++ precache-manifest.d7a89562f1db7ba772150e596808dd30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc489ed551e326549c27726e1e9ffd4",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "8c8b2808bd5a2d12cd96",
+    "url": "/recipe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98d21098c4df752ad10e",
+    "url": "/recipe/static/js/2.0809c235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe/static/js/2.0809c235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c8b2808bd5a2d12cd96",
+    "url": "/recipe/static/js/main.ce57d49a.chunk.js"
+  },
+  {
+    "revision": "1ba3af273f2546e19b34",
+    "url": "/recipe/static/js/runtime-main.c3119df6.js"
+  }
+]);
